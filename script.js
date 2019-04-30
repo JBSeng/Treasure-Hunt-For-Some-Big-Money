629371e76757df96f5ca708fffd5bb09e730399d
JBSeng <32690213+JBSeng@users.noreply.github.com>
2019-04-30 16:54:12 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,122 @@
+$(".Wrong").hide();
+$("#land").click(function(){
+    $("body").css("background","magenta");
+    $(".Wrong").show();
+    $(".spend").hide();
+    $("h2").hide();
+    $(".fairy").hide();
+    $(".shax").hide();
+    $("#cobra").hide();
+    $("#Yuno").hide();
+    $("#Asta").hide();
+    $("#Yama").hide();
+    $("#Golden").hide();
+    $(".question").hide();
+    $(".pants").hide();
+    $("#stop").hide();
+    $(".boys").hide();
+    $("#stop").hide();
+    $(".something").hide();
+    $(".cayde").hide();
+    $(".chopper").hide();
+    $(".luffy").hide();
+    $(".go").hide();
+    $(".last").hide();
+    $("h1").hide();
+    $("p").hide();
+    $(".jik").hide();
+    $("button").hide();
+    $(".apple").hide();
+    $(".cake").hide();
+});
+$(".fairy").hide();
+$(".shax").hide();
+$("#cobra").hide();
+$("#space").click(function() {
+    $(".fairy").show();
+    $(".shax").show();
+    $("#cobra").show();
+});
+$("#Yuno").hide();
+$("#Asta").hide();
+$("#Yama").hide();
+$("#Golden").hide();
+$(".question").hide();
+
+$("#cobra").click(function() {
+    $(".question").show();
+    $("#Yuno").show();
+    $("#Asta").show();
+    $("#Yama").show();
+    $("#Golden").show();
+});
+$(".pants").hide();
+$("#stop").hide();
+$(".boys").hide();
+$("#stop").hide();
+$("#Asta").click(function() {
+    $(".pants").show();
+    $("#stop").show();
+    $(".boys").show();
+});
+$(".something").hide();
+$("#jiraiya").dblclick(function(){
+    $(".something").show();
+});
+$(".cayde").hide();
+$(".something").mouseenter(function(){ 
+    $("body").css("background","pink");
+    $(".cayde").show();
+});
+$(".chopper").hide();
+$(".cayde").click(function() {
+    $(".chopper").show();
+});
+$(".luffy").hide();
+$(".chopper").mouseenter(function(){
+    $(".luffy").show();
+});
+$(".go").hide();
+$(".last").hide();
+$(".chopper").mouseleave(function(){
+    $(".go").show();
+    $(".last").show();
+});
+$("h2").hide();
+$(".go").click(function() {
+    $("h2").show();
+
+});
+$(".spend").hide();
+$(".luffy").click(function() {
+    $(".spend").show();
+    $("h2").show();
+    $(".fairy").hide();
+    $(".shax").hide();
+    $("#cobra").hide();
+    $("#Yuno").hide();
+    $("#Asta").hide();
+    $("#Yama").hide();
+    $("#Golden").hide();
+    $(".question").hide();
+    $(".pants").hide();
+    $("#stop").hide();
+    $(".boys").hide();
+    $("#stop").hide();
+    $(".something").hide();
+    $(".cayde").hide();
+    $(".chopper").hide();
+    $(".luffy").hide();
+    $(".go").hide();
+    $(".last").hide();
+    $("h1").hide();
+    $("p").hide();
+    $(".jik").hide();
+    $("button").hide();
+    $(".apple").hide();
+    $(".cake").hide();
+});
+
+
+
+
